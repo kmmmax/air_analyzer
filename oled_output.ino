230f20fdbd0d01f1b4d8f99ad8cea6ef818b259b
dex2code <kameneved@gmail.com>
2025-08-09 22:10:47 +0300

    Добавлено отключение OLED по таймауту

--- oled_output.ino
+++ oled_output.ino
@@ -32,7 +32,7 @@ void oled_values_output() {
   oled.setCursorXY(18, 24);
   oled.print(air_aht_temperature.temperature, 1);
   if (yellow_banner_temperature) {
-    oled.print("!");
+    oled.print(" !");
   }
 
   oled.setCursorXY(5, 47);
@@ -40,32 +40,33 @@ void oled_values_output() {
   oled.setCursorXY(18, 47);
   oled.print(air_aht_humidity.relative_humidity, 1);
   if (red_banner_humidity) {
-    oled.print("!");
+    oled.print(" !");
   }
 
-  oled.setCursorXY(69, 24);
+  oled.setCursorXY(63, 24);
   oled.print("CO2:");
-  oled.setCursorXY(94, 24);
+  oled.setCursorXY(88, 24);
   oled.print(air_scd_c02);
   if (yellow_banner_co2 || red_banner_co2) {
-    oled.print("!");
+    oled.print(" !");
   }
   
 
-  oled.setCursorXY(69, 47);
+  oled.setCursorXY(63, 47);
   oled.print("PM2:");
-  oled.setCursorXY(94, 47);
+  oled.setCursorXY(88, 47);
   oled.print(air_pm2);
   if (yellow_banner_pm2 || red_banner_pm2) {
-    oled.print("!");
+    oled.print(" !");
   }
 
   oled.fastLineH(16, 0, 127);
-  oled.fastLineH(63, 0, 127);
   oled.fastLineH(39, 0, 127);
+  oled.fastLineH(63, 0, 127);
+
   oled.fastLineV(0, 16, 63);
+  oled.fastLineV(58, 16, 63);
   oled.fastLineV(127, 16, 63);
-  oled.fastLineV(64, 16, 63);
 
   return;
 }

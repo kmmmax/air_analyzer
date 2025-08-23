bbc1168c1be099b92febea740c0bae4d90539c20
dex2code <kameneved@gmail.com>
2025-08-24 00:11:02 +0300

    Оптимизирован вывод JSON

--- serial_output.ino
+++ serial_output.ino
@@ -1,27 +1,11 @@
 void serial_output() {
-
   Serial.print("{ ");
-
-  Serial.print((String)"\"timestamp\":" + rtc_watch.Unix + ",");
-
-  Serial.print((String)"\"aht_temp\":" + "\"" + air_aht_temperature.temperature + "\"" + ",");
-  Serial.print((String)"\"bme_temp\":" + "\"" + air_bme_temperature + "\"" + ",");
-  Serial.print((String)"\"scd_temp\":" + "\"" + air_scd_temperature + "\"" + ",");
-
-  Serial.print((String)"\"aht_hum\":" + "\"" + air_aht_humidity.relative_humidity + "\"" + ",");
-  Serial.print((String)"\"bme_hum\":" + "\"" + air_bme_humidity + "\"" + ",");
-  Serial.print((String)"\"scd_hum\":" + "\"" + air_scd_humidity + "\"" + ",");
-
+  Serial.print((String)"\"time\":" + rtc_watch.Unix + ",");
+  Serial.print((String)"\"temp\":" + "\"" + air_aht_temperature.temperature + "\"" + ",");
+  Serial.print((String)"\"hum\":" + "\"" + air_aht_humidity.relative_humidity + "\"" + ",");
   Serial.print((String)"\"co2\":" + air_scd_co2 + ",");
-
   Serial.print((String)"\"pm2\":" + air_pm2 + ",");
-
+  Serial.print((String)"\"pa\":" + "\"" + air_bme_pressure_pa + "\"");
   Serial.println(" }");
-
-/*
-  Serial.print(air_bme_pressure_pa); Serial.print(";");
-  Serial.println(air_bme_pressure_hg);
-*/
-
   return;
 }
